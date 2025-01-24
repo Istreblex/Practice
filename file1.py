050ae140fb9d28961507e33399c21f10e0c3ecb5
NovikovAL <aleksejnovikov776@gmail.com>
2025-01-24 13:01:56 +0500

    Изменен file1.py, добавлены комментарии

--- file1.py
+++ file1.py
@@ -9,10 +9,11 @@ step = 0.33
 
 print("Табулирование f1(x) = sqrt(sin²x + cos⁴x) на [-3.4; 3.4] с шагом 0.33:")
 x = start
+#Алгоритм функции
 while x <= end:
     try:
         result = round(f1(x), 4)
         print(f"x = {round(x, 2):>5}, f1(x) = {result}")
     except ValueError:
         print(f"x = {round(x, 2):>5}, f1(x) не определена (подкоренное выражение < 0)")
-    x += step
+    x += step #Отступ на шаг
